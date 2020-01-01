8a8b9e1f1c5c3787d6a1f16f54e83527a554f884
Gedan Magalhães <gedanycar@gmail.com>
2020-01-01 12:07:08 -0300

    Adicionando a inteface OnChanges

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html'
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
+  rows: any[] = []
+
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges){
+    if(changes.photos){
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
   ngOnInit() {
+
+    this.rows = this.groupColumns(this.photos);
+  }
+
+  groupColumns(photos: Photo[]){
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index+= 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
   }
 
 }
