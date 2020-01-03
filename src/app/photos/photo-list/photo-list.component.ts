bb33c178ec74389a6f2b9b6aafee9327938efdc2
Gedan Magalhães <gedanycar@gmail.com>
2020-01-02 22:24:13 -0300

    Implementando o debounce na pesquisa de fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@Angular/router";
+import { Subject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+
 
 import { Photo } from "../photo/photo";
 
@@ -12,10 +15,20 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+
 
   constructor(public activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+
+    this.debounce
+        .pipe(debounceTime(300))
+        .subscribe(filter => this.filter = filter);
   }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+}
 }
